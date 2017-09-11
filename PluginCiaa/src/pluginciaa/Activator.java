574259dac9657c41739d14683de1b0c16f44041c
freydeb <frey@debmedia.com>
2017-09-11 14:59:52 -0300

    test22

--- PluginCiaa/src/pluginciaa/Activator.java
+++ PluginCiaa/src/pluginciaa/Activator.java
@@ -5,7 +5,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle Felipe
+ * The activator class controls the plug-in life cycle Felipe REy
  */
 public class Activator extends AbstractUIPlugin {
 
